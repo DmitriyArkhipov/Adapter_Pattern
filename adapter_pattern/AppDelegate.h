e7314a0edbc21ed5be8a884bdf6846d2de3406e5
Dmitriy Arkhipov <dmitriy.arkhipov.biz@mail.ru>
2016-08-19 19:19:40 +0800

    Initial Commit

---
+++ adapter_pattern/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  adapter_pattern
+//
+//  Created by Dmitriy Arkhipov on 19.08.16.
+//  Copyright © 2016 Dmitriy Arkhipov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
